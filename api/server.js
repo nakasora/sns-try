bb6d01021b541d82f6e201546616fc524cf8c870
nakasora <n17302826@gmail.com>
2023-11-04 15:55:29 +0900

    feat/superbase疎通確認の完了

--- api/server.js
+++ api/server.js
@@ -1,6 +1,25 @@
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
 const express = require("express");
 const app = express();
 
 const PORT = 5000;
 
+const prisma = new PrismaClient();
+app.use(express.json());
+
+//新規ユーザ登録API
+app.post("/api/auth/register", async (req, res) => {
+  console.log(req.body);
+  const { username, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+  return res.json({ user });
+});
 app.listen(PORT, () => console.log("server start"));
